120dee2bcfcc4275b3ebaf320d2a7fc1d8bfda65
ryanmody <rahimmody488@gmail.com>
2024-07-06 20:11:35 -0400

    Used buttons instead to allow for player choices, and now there's counters for wins and how many rounds played, and the game ends after 5 rounds

--- script.js
+++ script.js
@@ -5,7 +5,21 @@ let roundCounter = 0;
 let playerWins = 0;
 let computerWins = 0;
 
+let playerWinsText = document.querySelector("#playerWins");
+let computerWinsText = document.querySelector("#computerWins");
+let roundCounterText = document.querySelector("#roundCounter");
 
+const buttonrock = document.querySelector("#buttonrock");
+const buttonpaper = document.querySelector("#buttonpaper");
+const buttonscissors = document.querySelector("#buttonscissors");
+
+// Initializing the buttons
+buttonrock.onclick = rockVsComputer;
+buttonpaper.onclick = paperVsComputer;
+buttonscissors.onclick = scissorsVsComputer;
+
+
+// Computer picks Rock, Paper, or Scissors
 function computerPicksRPS() {
     computerChoiceNumber = Math.floor(Math.random() * 3 + 1);
     if (computerChoiceNumber == 1) {
@@ -16,47 +30,89 @@ function computerPicksRPS() {
         computerChoiceName = "Scissors";
     }
 }
-
 computerPicksRPS();
-
 console.log(computerChoiceName);
 
+
+// Player makes their choice via buttons
+function rockVsComputer() {
+    playerChoice = "Rock"
+    playerVsComputer();
+}
+
+function paperVsComputer() {
+    playerChoice = "Paper"
+    playerVsComputer();
+}
+
+function scissorsVsComputer() {
+    playerChoice = "Scissors"
+    playerVsComputer();
+}
+
+
 function playerVsComputer () {
-    // Code for player choosing RPS, and determining outcome
-    let playerChoice = prompt("Choose Rock, Paper, or Scissors");
+// Code for player choosing RPS, and determining outcome
     if (playerChoice == "Rock" && computerChoiceName == "Scissors") {
         alert("You win!");
         playerWins++;
         roundCounter++;
+        computerPicksRPS();
+        playerWinsText.innerText = playerWins;
+        roundCounterText.innerText = roundCounter;
+        computerWinsText.innerText = computerWins;
     } else if (playerChoice == "Rock" && computerChoiceName == "Paper") {
         alert("You lose!");
         computerWins++;
         roundCounter++;
+        computerPicksRPS();
+        playerWinsText.innerText = playerWins;
+        roundCounterText.innerText = roundCounter;
+        computerWinsText.innerText = computerWins;
     } else if (playerChoice == "Paper" && computerChoiceName == "Rock") {
         alert("You win!");
         playerWins++;
         roundCounter++;
+        computerPicksRPS();
+        playerWinsText.innerText = playerWins;
+        roundCounterText.innerText = roundCounter;
+        computerWinsText.innerText = computerWins;
     } else if (playerChoice == "Paper" && computerChoiceName == "Scissors") {
         alert("You lose!");
         computerWins++;
         roundCounter++;
+        computerPicksRPS();
+        playerWinsText.innerText = playerWins;
+        roundCounterText.innerText = roundCounter;
+        computerWinsText.innerText = computerWins;
     } else if (playerChoice == "Scissors" && computerChoiceName == "Paper") {
         alert("You win!");
         playerWins++;
         roundCounter++;
+        computerPicksRPS();
+        playerWinsText.innerText = playerWins;
+        roundCounterText.innerText = roundCounter;
+        computerWinsText.innerText = computerWins;
     } else if (playerChoice == "Scissors" && computerChoiceName == "Rock") {
         alert("You lose!");
         computerWins++;
         roundCounter++;
+        computerPicksRPS();
+        playerWinsText.innerText = playerWins;
+        roundCounterText.innerText = roundCounter;
+        computerWinsText.innerText = computerWins;
     } else {
         alert("It's a tie!");
+        roundCounter++;
+        roundCounterText.innerText = roundCounter;
+    }
+
+    //Code for checking if the game is done
+    if (roundCounter > 4) {
+        if (playerWins > computerWins) {
+            alert("You won the set");
+        } else if (playerWins < computerWins) {
+            alert("You lost the set");
+        } else {}
     }
 }
-
-playerVsComputer();
-computerPicksRPS();
-
-console.log(computerWins);
-console.log(playerWins);
-console.log(roundCounter);
-
